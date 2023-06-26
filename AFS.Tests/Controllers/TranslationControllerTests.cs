8e977bf7d7c9f3a7d7221d0861f7dbc450fe2143
UlviDemirsoy <tedulvi@gmail.com>
2023-06-27 02:58:39 +0300

    Unit Tests
    Unit Tests

---
+++ AFS.Tests/Controllers/TranslationControllerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFS.Tests.Controllers
+{
+    public class TranslationControllerTests
+    {
+    }
+}
