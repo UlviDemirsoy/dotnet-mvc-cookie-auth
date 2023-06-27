3ceca3242d6f75dad3a5c317cba835bab7814dd7
UlviDemirsoy <tedulvi@gmail.com>
2023-06-27 13:13:23 +0300

    unit tests

--- AFS.Tests/Controllers/TranslationControllerTests.cs
+++ AFS.Tests/Controllers/TranslationControllerTests.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using AFS.Controllers;
+using AFS.Dtos;
+using AFS.Models;
+using AFS.Repositories.Abstract;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +13,52 @@ using System.Threading.Tasks;
 
 namespace AFS.Tests.Controllers
 {
+
     public class TranslationControllerTests
     {
+        private TranslationController _translationController;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private ITranslationService _translationService;
+
+        public TranslationControllerTests()
+        {
+            //Dependencies
+            _translationService = A.Fake<ITranslationService>();
+            _httpClientFactory = A.Fake<IHttpClientFactory>();
+
+
+            //SUT
+            _translationController = new TranslationController(_translationService, _httpClientFactory);
+        }
+
+        [Fact]
+        public void TranlationController_Index_ReturnsSucces()
+        {
+            //Arrange
+            var translations = A.Fake<TranslationListVm>();
+            string searchterm = "";
+            bool paging = false;
+            int currentpage = 0;
+            A.CallTo(() => _translationService.List(searchterm, paging, currentpage)).Returns(translations);
+
+            //Act
+
+            var result = _translationController.Index();
+
+            //Assert
+
+            result.Should().BeOfType<IActionResult>();
+
+
+        }
+
+
+
     }
+
+
+
+
+
+
 }
